4933345d303d2ce87dcf54814d12441a66c1460b
GayatriGuldagad07 <104750891+GayatriGuldagad07@users.noreply.github.com>
2022-05-06 10:07:05 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,73 @@
+//Question 1  : Addition ,subtraction,division and multiplication
+
+// function Calculator()
+// {
+// 	var a=20;
+// 	var b=10;
+
+// 	var addition = () =>
+// 	{
+// 		console.log(a+b);
+// 	}
+
+// 	addition();
+
+// 	var subtraction = () =>
+// 	{
+// 		console.log(a-b);
+// 	}
+
+// 	subtraction();
+
+// 	var multiplication = () =>
+// 	{
+// 		console.log(a*b);
+// 	}
+
+// 	multiplication();
+
+// 	var division = () =>
+// 	{
+// 		console.log(a/b);
+// 	}
+
+// 	division();
+
+// 	return "Gayatri";
+// }
+
+// var calci=Calculator();
+// console.log(calci)
+
+//Question 2
+
+var AreaofCircle = () => {
+  var r = 10;
+  var area;
+  var findout = () => {
+    area = 3.14 * r * r;
+  };
+  findout();
+  return area;
+};
+
+var circlearea = AreaofCircle();
+console.log(circlearea);
+
+//Question 3
+
+// var temp = 20;
+// const outerFunction = () => {
+//   var outerVar1 = "javascript";
+//   console.log(temp);
+
+//   function innerFunction() {
+//     console.log(temp);
+//     // var temp = 345;
+//     console.log(temp);
+//   }
+
+//   innerFunction();
+// };
+
+// outerFunction();
